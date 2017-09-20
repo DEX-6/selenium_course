a57202dfdd12f5d0188d940d36a13356fde8fc16
DEX-6 <irina14.91@bk.ru>
2017-09-20 18:47:37 +0400

    добавление поиска элементов главного меню

--- java-example/src/test/java/ru/stqa/training/selenium/MenuItemsClickTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MenuItemsClickTest.java
@@ -4,13 +4,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MenuItemsClickTest {
@@ -46,6 +49,17 @@ public class MenuItemsClickTest {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         wait.until(ExpectedConditions.titleContains("My Store"));
+
+        List <WebElement> mainMenuItem = driver.findElements(By.xpath("//div[@id = 'box-apps-menu-wrapper']//li[@id = 'app-']"));
+        int mainMenuItemCount = mainMenuItem.size();
+        System.out.println(" mainMenuItemCount = " + mainMenuItemCount);
+
+        for (int i = 0; i < mainMenuItemCount; i++ ){
+//            mainMenuItem.get(i).click();
+//            ((JavascriptExecutor)driver).executeScript()
+            driver.findElements(By.xpath("//div[@id = 'box-apps-menu-wrapper']//li[@id = 'app-']")).get(i).click();
+            System.out.println("Click: " + i + "\\n");
+        }
     }
 
     @After
