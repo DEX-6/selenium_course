bb73c33023cd371d53118b4fa9bfbe9f64268550
DEX-6 <irina14.91@bk.ru>
2017-10-03 15:46:26 +0400

    Тест проверки сортировки Стран и геозон

--- java-example/src/test/java/ru/stqa/training/selenium/CountryAndZonesSortCheckTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CountryAndZonesSortCheckTest.java
@@ -8,24 +8,25 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class CountrySortCheckTest extends TestBase {
+public class CountryAndZonesSortCheckTest extends TestBase {
 
     @FindBy(xpath = "//form[@name='countries_form']//tr[@class='row']")
     List<WebElement> countryRow;
 
+    @FindBy(xpath = "//table[@id = 'table-zones']//tr")
+    List<WebElement> zonesRow;
+
     List<String> countryListNaturale = new ArrayList<String>();
-    //    List<WebElement> countryListNaturale;
     List<String> countryListSorted;
 
     List<String> zones = new ArrayList<String>();
     List<Integer> notNullZones = new ArrayList<Integer>();
-
-    @FindBy(xpath = "//table[@id = 'table-zones']//tr")
-    List<WebElement> zonesRow;
+    
+    List<String> zoneNature;
+    List<String> zoneSorted;
 
     @Test
     public void countrySortCheckTest() {
@@ -36,8 +37,7 @@ public class CountrySortCheckTest extends TestBase {
         wait.until(ExpectedConditions.titleContains("Countries | My Store"));
 
         int countryCount = countryRow.size();
-        System.out.println(countryCount);
-
+        System.out.println("Количество стран: " + countryCount);
 //        Получение списка стран
         for (int i = 0; i < countryCount; i++) {
             countryListNaturale.add(driver
@@ -61,31 +61,33 @@ public class CountrySortCheckTest extends TestBase {
             zones.add(driver.findElement(By.xpath("//form[@name='countries_form']//tr[@class='row'][" + (i + 1) + "]//td[6]")).getText());
             if (!zones.get(i).equals("0")) {
                 notNullZones.add(i + 1);
+                System.out.println("Номер строки с непустой зоной: " + notNullZones.get(notNullZones.size() - 1));
             }
         }
 
 //        Проверка стран, у которых количество зон больше нуля
-/*        for (int i = 0; i < notNullZones.size(); i++) {
-            List<WebElement> countryWithZone = driver.findElements(By.xpath("//form[@name='countries_form']//tr[@class='row'][" + notNullZones.get(i) + "]//td[5]"));
-            for (int j = 0; j < countryWithZone.size(); j ++){
-                countryWithZone.get(j).click();
+        for (int j = 0; j < notNullZones.size(); j++) {
+            WebElement countryWithZone = driver.findElement(By.xpath("//form[@name='countries_form']//tr[@class='row'][" + notNullZones.get(j) + "]//td[5]//a"));
+            countryWithZone.click();
 
+            wait.until(ExpectedConditions.titleContains("Edit Country | My Store"));
+            zoneNature = new ArrayList<>();
+
+//            Проверка сортировки зон по алфавиту
+            {
+                for (int i = 0; i < zonesRow.size() - 2; i++) {
+                    zoneNature.add(driver.findElement(By.xpath("//table[@id = 'table-zones']//tr[" + (i + 2) + "]//td[3]")).getText());
+                    System.out.println("Зона " + notNullZones.get(j) + ":" + zoneNature.get(i));
+                }
+                zoneSorted = new ArrayList<String>(zoneNature);
+                Collections.sort(zoneSorted);
+
+                Assert.assertTrue("Списки не равны!", zoneNature.equals(zoneSorted));
+
+                driver.navigate().back();
+                wait.until(ExpectedConditions.titleContains("Countries | My Store"));
             }
 
-//            driver.navigate().back();
-        }*/
-
-        List<WebElement> countryWithZone = driver.findElements(By.xpath("//form[@name='countries_form']//tr[@class='row'][" + notNullZones.get(1) + "]//td[5]"));
-        countryWithZone.get(1).click();
-
-        wait.until(ExpectedConditions.titleContains("Edit Country | My Store"));
-
-//            Получение списка зон
-        for (int i = 0; i < zonesRow.size(); i++) {
-//            driver.findElements("")
-
         }
-
-
     }
 }
