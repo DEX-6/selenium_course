fb96d2547ed22df9b0a235a64d8c2ddf0f4bbabc
DEX-6 <irina14.91@bk.ru>
2017-10-08 22:15:44 +0300

    Изменение теста
    1. Проверка цвета обычной цены
    2. Проверка соответствия названия, цены (обычной, акционной) на главной
    странице и странице продукта

--- java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/PageCheckTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/PageCheckTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.stqa.training.selenium.pageObject.Pages.ProductPage;
 import ru.stqa.training.selenium.pageObject.Tests.TestBase;
 import ru.stqa.training.selenium.pageObject.Pages.MainPage;
 
@@ -18,7 +19,6 @@ public class PageCheckTest extends TestBase {
 
 
 
-
     @Test
     public void pageCheckTest() {
         driver.get("http://localhost/litecart/en/");
@@ -27,46 +27,49 @@ public class PageCheckTest extends TestBase {
 //        Более точно, нужно открыть главную страницу, выбрать первый товар в блоке Campaigns и проверить следующее:
         MainPage mainPage = new MainPage(driver);
 
-        mainPage.getMainProductName();
-        mainPage.getMainProductPrice();
-        mainPage.getMainProductPriceWithSale();
+        String mainProductName =  mainPage.getMainProductName();
+        String mainProductPrice =  mainPage.getMainProductPrice();
+        String mainProductPriceWithSale =  mainPage.getMainProductPriceWithSale();
 
 //        Проверка цвета акционной цены
-        System.out.println("green: " + mainPage.mainPriceColorSector(salePrice, green));
-        System.out.println("blue: " + mainPage.mainPriceColorSector(salePrice, blue));
+        System.out.println("Акционная цена, красный: " + mainPage.mainPriceColorSector(salePrice, red));
+        System.out.println("Акционная цена, зеленый: " + mainPage.mainPriceColorSector(salePrice, green));
+        System.out.println("Акционная цена, синий: " + mainPage.mainPriceColorSector(salePrice, blue));
         Assert.assertTrue("Цвет акционной цены не является красным!",
                 mainPage.mainPriceColorSector(salePrice, green) == 0 && mainPage.mainPriceColorSector("sale", "B") == 0);
 
-        System.out.println("обычная цена красный:" + mainPage.mainPriceColorSector(generalPrice, red));
-        System.out.println("обычная цена зеленый:" + mainPage.mainPriceColorSector(generalPrice, green));
-        System.out.println("обычная цена синий:" + mainPage.mainPriceColorSector(generalPrice, blue));
-        // TODO: 06.10.2017 Доделать проверку серого цета для обычной цены
+
+//        Проверка цвета обычной цены
+        System.out.println("Обычная цена, красный: " + mainPage.mainPriceColorSector(generalPrice, red));
+        System.out.println("Обычная цена, зеленый: " + mainPage.mainPriceColorSector(generalPrice, green));
+        System.out.println("Обычная цена, синий: " + mainPage.mainPriceColorSector(generalPrice, blue));
         Assert.assertTrue("Цвет обычной цены не является серым!",
-                (mainPage.mainPriceColorSector(generalPrice, red)==mainPage.mainPriceColorSector(generalPrice, green))
-                        &&(mainPage.mainPriceColorSector(generalPrice, green)==mainPage.mainPriceColorSector(generalPrice, blue)));
+                (mainPage.mainPriceColorSector(generalPrice, red) == mainPage.mainPriceColorSector(generalPrice, green))
+                        && (mainPage.mainPriceColorSector(generalPrice, green) == mainPage.mainPriceColorSector(generalPrice, blue)));
 //        Переход на страницу продукта
         mainPage.goToProductPage();
         wait.until(ExpectedConditions.titleContains("Yellow Duck | Subcategory | Rubber Ducks | My Store"));
-//
-//
-//
-//
-////        Проверки
-////        а) на главной странице и на странице товара совпадает текст названия товара
-//        Assert.assertTrue("Название товара на главной странице \"" + mainProductName + "\" не совпадает с названием на странице товара \"" + subProductName + "\"!",
-//                mainProductName.equals(subProductName));
-//
-////        б) на главной странице и на странице товара совпадают цены (обычная и акционная)
-//        Assert.assertTrue("Обычная цена товара на главной странице \"" + mainProductPrice + "\" не совпадает с обычной ценой на странице товара \"" + subProductPrice + "\"!",
-//                mainProductPrice.equals(subProductPrice));
-//
-//        Assert.assertTrue("Акционная цена товара на главной странице \"" + mainProductPriceWithSale + "\" не совпадает с акционной ценой на странице товара \"" + subProductPriceWithSale + "\"!",
-//                mainProductPriceWithSale.equals(subProductPriceWithSale));
-//
-////        в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
-////        г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa представлении каналы G и B имеют нулевые значения)
-////        (цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
+
+        ProductPage productPage = new ProductPage(driver);
+
+//        Проверки
+//        а) на главной странице и на странице товара совпадает текст названия товара
+        Assert.assertTrue("Название товара на главной странице \"" + mainProductName + "\" не совпадает с названием на странице товара \"" + productPage.getSubProductName() + "\"!",
+                mainProductName.equals(productPage.getSubProductName()));
+
+//        б) на главной странице и на странице товара совпадают цены (обычная и акционная)
+        Assert.assertTrue("Обычная цена товара на главной странице \"" + mainProductPrice + "\" не совпадает с обычной ценой на странице товара \"" + productPage.getSubProductPrice() + "\"!",
+                mainProductPrice.equals(productPage.getSubProductPrice()));
+
+        Assert.assertTrue("Акционная цена товара на главной странице \"" + mainProductPriceWithSale + "\" не совпадает с акционной ценой на странице товара \"" + productPage.getSubProductPriceWithSale() + "\"!",
+                mainProductPriceWithSale.equals(productPage.getSubProductPriceWithSale()));
+
+//        в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
+//        г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa представлении каналы G и B имеют нулевые значения)
+//        (цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
 //        г) акционная цена крупнее, чем обычная (это тоже надо проверить на каждой странице независимо)
 
     }
+
+//    rgba(119, 119, 119, 1)
 }
