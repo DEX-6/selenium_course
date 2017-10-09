b2aa62fa75d7612a15ba94c67f6a84928e3d2c63
DEX-6 <irina14.91@bk.ru>
2017-10-09 15:54:13 +0400

    DebugTest: получение размера цены

--- java-example/src/test/java/ru/stqa/training/selenium/DebugTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/DebugTest.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//public class DebugTest extends TestBase {
-public class DebugTest {
+public class DebugTest extends TestBase {
+//public class DebugTest {
 
     @Test
     public void debugTest() {
@@ -29,19 +29,40 @@ public class DebugTest {
 //        System.out.println("colorG: " + colorG);
 //        System.out.println("colorB: " + colorB);
 //        rgba(204, 0, 0, 1)
+//
+//        String color = "rgba(119, 119, 119, 1)";
+//        System.out.println("color: " + color);
+//        String colorRGB;
+//
+//
+////        String [] colorRGB = color.split("([0-9]{1,}, [0-9]{1,}, [0-9]{1,})");
+////        String colorRGB = String.format("([0-9]{1,}, [0-9]{1,}, [0-9]{1,})", color);
+//        Pattern pattern = Pattern.compile("[0-9]{1,}, [0-9]{1,}, [0-9]{1,}");
+//        Matcher matcher = pattern.matcher(color);
+//        if (matcher.find()){
+//            colorRGB = matcher.group();
+//        } else colorRGB = "цвет не найден";
+//        System.out.println("colorRGB: " + colorRGB);
 
-        String color = "rgba(119, 119, 119, 1)";
-        System.out.println("color: " + color);
-        String colorRGB;
+        driver.get("http://localhost/litecart/en/");
+        wait.until(ExpectedConditions.titleContains("Online Store | My Store"));
 
+        String generalLine = driver.findElement(By.xpath("//div[@id = 'box-campaigns']//li//s[@class='regular-price']"))
+                .getCssValue("font-size");
+        System.out.println( "general: " + generalLine);
 
-//        String [] colorRGB = color.split("([0-9]{1,}, [0-9]{1,}, [0-9]{1,})");
-//        String colorRGB = String.format("([0-9]{1,}, [0-9]{1,}, [0-9]{1,})", color);
-        Pattern pattern = Pattern.compile("[0-9]{1,}, [0-9]{1,}, [0-9]{1,}");
-        Matcher matcher = pattern.matcher(color);
+        String salelLine = driver.findElement(By.xpath("//div[@id = 'box-campaigns']//li//strong[@class='campaign-price']"))
+                .getCssValue("font-size");
+
+        System.out.println("sale: " + salelLine);
+
+        String fontSize = driver.findElement(By.xpath("//div[@id = 'box-campaigns']//li//strong[@class='campaign-price']")).getCssValue("font-size");
+        String priceSize;
+        Pattern pattern = Pattern.compile("[0-9]{1,}[.0-9]{0,}");
+        Matcher matcher = pattern.matcher(fontSize);
         if (matcher.find()){
-            colorRGB = matcher.group();
-        } else colorRGB = "цвет не найден";
-        System.out.println("colorRGB: " + colorRGB);
-    }
+            priceSize = matcher.group();
+        } else priceSize = "Не удалось узнать размер шрифта";
+
+        System.out.println(" priceSize: " + Double.parseDouble(priceSize));    }
 }
