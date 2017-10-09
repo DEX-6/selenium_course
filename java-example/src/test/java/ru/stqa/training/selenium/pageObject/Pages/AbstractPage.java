de4e4bfab367c7d3c664e887373913e615d13fbf
DEX-6 <irina14.91@bk.ru>
2017-10-09 15:57:45 +0400

    Изменение модификатора доступа к методам

--- java-example/src/test/java/ru/stqa/training/selenium/pageObject/Pages/AbstractPage.java
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Pages/AbstractPage.java
@@ -8,14 +8,14 @@ import java.util.regex.Pattern;
 public class AbstractPage {
 
 
-    public String getPriceColorSector(WebElement element, String RGBSector) {
+    String getPriceColorSector(WebElement element, String RGBSector) {
         //        в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
 //        Проверка цвета акционной цены
         String color = element.getCssValue("color");
         String colorRGB;
         Pattern pattern = Pattern.compile("[0-9]{1,}, [0-9]{1,}, [0-9]{1,}");
         Matcher matcher = pattern.matcher(color);
-        if (matcher.find()){
+        if (matcher.find()) {
             colorRGB = matcher.group();
         } else colorRGB = "Цвет не найден";
         switch (RGBSector) {
@@ -32,12 +32,12 @@ public class AbstractPage {
         }
     }
 
-    public String getPriceSize(WebElement price){
+    String getPriceSize(WebElement price) {
         String fontSize = price.getCssValue("font-size");
         String priceSize;
         Pattern pattern = Pattern.compile("[0-9]{1,}[.0-9]{0,}");
         Matcher matcher = pattern.matcher(fontSize);
-        if (matcher.find()){
+        if (matcher.find()) {
             priceSize = matcher.group();
         } else priceSize = "Не удалось узнать размер шрифта";
         return priceSize;
