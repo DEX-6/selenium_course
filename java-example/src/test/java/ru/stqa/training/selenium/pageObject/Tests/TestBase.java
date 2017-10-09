01fc0f22894518196e3d16e4d8dd0773ae551cac
DEX-6 <irina14.91@bk.ru>
2017-10-09 23:23:08 +0300

    Добавление переменной определяющей название браузера

--- java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/TestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,7 +20,9 @@ public class TestBase {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
-    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    private DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    public static String browserType;
+
 
     public void init(String browserName) {
         if (browserName.equals("Chrome")) {
@@ -27,33 +30,38 @@ public class TestBase {
             chromeOptions.addArguments("start-maximized");
             desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
             driver = new ChromeDriver(desiredCapabilities);
+            browserType = BrowserType.CHROME;
         } else if (browserName.equals("InternetExplorer")) {
             driver = new InternetExplorerDriver();
             driver.manage().window().maximize();
+            browserType = BrowserType.IE;
         } else if (browserName.equals("Firefox Old Scheme Caps")) {
             desiredCapabilities.setCapability(FirefoxDriver.MARIONETTE, false);
             driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")), new FirefoxProfile(), desiredCapabilities);
             driver.manage().window().maximize();
+            browserType = BrowserType.FIREFOX;
         } else if (browserName.equals("Firefox Old Scheme Options")) {
             FirefoxOptions options = new FirefoxOptions();
             options.setLegacy(false);
             options.setBinary("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
             driver = new FirefoxDriver(options);
             driver.manage().window().maximize();
+            browserType = BrowserType.FIREFOX;
         } else if (browserName.equals("Firefox New Scheme")) {
             driver = new FirefoxDriver();
             driver.manage().window().maximize();
+            browserType = BrowserType.FIREFOX;
         }
     }
 
     @Before
     public void start() {
         {
-            init("Chrome");
+//            init("Chrome");
 //            init("InternetExplorer");
 //            init("Firefox Old Scheme Caps");
 //            init("Firefox Old Scheme Options");
-//            init("Firefox New Scheme");
+            init("Firefox New Scheme");
             wait = new WebDriverWait(driver, 10);
         }
     }
