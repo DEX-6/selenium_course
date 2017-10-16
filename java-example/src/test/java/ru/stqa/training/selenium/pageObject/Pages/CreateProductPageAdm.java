d2f3e401d234836adba83afee446f7b2fd8db054
DEX-6 <irina14.91@bk.ru>
2017-10-16 23:12:16 +0400

    Страница создания продукта

---
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Pages/CreateProductPageAdm.java
@@ -0,0 +1,44 @@
+package ru.stqa.training.selenium.pageObject.Pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.File;
+
+public class CreateProductPageAdm extends AbstractPage {
+
+    WebDriver driver;
+    String productName;
+
+    public CreateProductPageAdm(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void topMenuItemClick(String topMenuItemName) {
+        driver.findElement(By.xpath("//ul[@class='index']//a[contains(text(), '" + topMenuItemName + "')]")).click();
+        Assert.assertTrue("Вкладка меню не была выбрана!", driver.findElement(By.xpath("//ul[@class='index']//a[contains(text(), '" + topMenuItemName + "')]/..")).getAttribute("class").equals("active"));
+    }
+
+    public void fillField(String fieldName, String text) {
+        driver.findElement(By.xpath("//div[@class='content']//input[@name='" + fieldName + "']")).sendKeys();
+    }
+
+    public void downLoadPic() {
+        File file = new File("cats.jpg");
+        driver.findElement(By.xpath("//input[@name='new_images[]']")).sendKeys(file.getAbsolutePath());
+    }
+
+    public String createProductName() {
+        productName = "Product №" + uniqueNumberGenerator(10);
+        return productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+}
