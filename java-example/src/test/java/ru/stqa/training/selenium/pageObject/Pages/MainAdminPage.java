db13f9881de812cd0cf44e30a151f60a313e6b74
DEX-6 <irina14.91@bk.ru>
2017-10-16 23:11:47 +0400

    Главная страница админки, клики бокового меню, инициация создания продукта

--- java-example/src/test/java/ru/stqa/training/selenium/pageObject/Pages/MainAdminPage.java
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Pages/MainAdminPage.java
@@ -8,25 +8,28 @@ public class MainAdminPage extends AbstractPage {
 
     WebDriver driver;
 
-    public MainAdminPage(WebDriver driver){
+    public MainAdminPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
-    public void login(){
+    public void login() {
 
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
     }
 
-    public void mainMenuItemClick(){
-        driver.findElement(By.xpath("//div[@id = 'box-apps-menu-wrapper']//li[@id = 'app-']//span[contains(text(), 'Catalog')]")).click();
+    public void mainMenuItemClick(String mainMenuName) {
+        driver.findElement(By.xpath("//div[@id = 'box-apps-menu-wrapper']//li[@id = 'app-']//span[contains(text(), '" + mainMenuName + "')]")).click();
     }
 
-    public void subMenuItemClick(){
-        driver.findElement(By.xpath(""));
+    public void subMenuItemClick(String subMenuName) {
+        driver.findElement(By.xpath("//div[@id = 'box-apps-menu-wrapper']//li[@id = 'app-']//ul[@class = 'docs']//li[contains(@id, 'doc-')]//span[contains(text(), '" + subMenuName + "')]"));
     }
 
+    public void initCreateProduct() {
+        driver.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/?category_id=0&app=catalog&doc=edit_product']")).click();
+    }
 }
