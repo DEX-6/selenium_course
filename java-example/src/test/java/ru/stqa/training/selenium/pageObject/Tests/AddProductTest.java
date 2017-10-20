5872de4988e8ed55c7d3aa2a04b5a9cd0913cee8
DEX-6 <irina14.91@bk.ru>
2017-10-20 16:29:41 +0400

    + заполнение вкладок продукта

--- java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/AddProductTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/pageObject/Tests/AddProductTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.training.selenium.pageObject.Tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.stqa.training.selenium.pageObject.Pages.CreateProductPageAdm;
@@ -7,8 +8,10 @@ import ru.stqa.training.selenium.pageObject.Pages.MainAdminPage;
 
 public class AddProductTest extends TestBase {
 
+    Thread thread = new Thread();
+
     @Test
-    public void addProductTest() {
+    public void addProductTest() throws InterruptedException {
 //        Вход
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
         MainAdminPage mainAdminPage = new MainAdminPage(driver);
@@ -25,23 +28,90 @@ public class AddProductTest extends TestBase {
         mainAdminPage.initCreateProduct();
         wait.until(ExpectedConditions.titleContains("Add New Product | My Store"));
 
-//        Заполнение вкладки General
         CreateProductPageAdm createProductPageAdm = new CreateProductPageAdm(driver);
         createProductPageAdm.createProductName();
+//        Заполнение вкладки General
 
-        createProductPageAdm.topMenuItemClick("General");
+//        Status
         createProductPageAdm.radioButtonClick(" Enabled");
-//        createProductPageAdm.fillField("name[en]", createProductPageAdm.getProductName());
+//        Name
+        createProductPageAdm.fillField("name[en]", createProductPageAdm.getProductName());
+//        Code
         createProductPageAdm.fillField("code", createProductPageAdm.getProductName());
-        createProductPageAdm.categoryCheckboxClick("Root");
-        createProductPageAdm.categoryCheckboxClick("Rubber Ducks");
-//        createProductPageAdm.downLoadPic();
-        createProductPageAdm.productGroupsCheckboxClick("Female");
+//        Categories
+        createProductPageAdm.categoryCheckboxClick("Root", "-");
+        createProductPageAdm.categoryCheckboxClick("Rubber Ducks", "+");
+
+//        Default Category
+        createProductPageAdm.fillCombobox("default_category_id", "Rubber Ducks");
+//        Product Groups
+        createProductPageAdm.productGroupsCheckboxClick("Female", "+");
+        createProductPageAdm.productGroupsCheckboxClick("Male", "-");
+
+//        Quantity
+        createProductPageAdm.fillField("quantity", "15,00");
+
+//        Quantity Unit
+        createProductPageAdm.fillCombobox("quantity_unit_id", "pcs");
+
+//        Delivery Status
+        createProductPageAdm.fillCombobox("delivery_status_id", "3-5 days");
+
+//        Sold Out Status
+        createProductPageAdm.fillCombobox("sold_out_status_id", "Temporary sold out");
+
+//        Upload Images
+        createProductPageAdm.downLoadPic();
+
+//        Date Valid From
+        createProductPageAdm.fillField("date_valid_from", "01.01.2018");
+
+//        Date Valid To
+        createProductPageAdm.fillField("date_valid_to", "01.01.2020");
+
+
 //        Заполнение вкладки Information
-//        createProductPageAdm.topMenuItemClick("Information");
-//        createProductPageAdm.fillField("short_description[en]", createProductPageAdm.getProductName());
-//        createProductPageAdm.fillTextArea("trumbowyg-editor", String.valueOf(createProductPageAdm.getProductName()));
-//        createProductPageAdm.fillField("head_title[en]", createProductPageAdm.getProductName());
-//        createProductPageAdm.fillField("meta_description[en]", createProductPageAdm.getProductName());
+        createProductPageAdm.topMenuItemClick("Information");
+
+//        Manufacturer
+        createProductPageAdm.fillCombobox("manufacturer_id", "ACME Corp.");
+
+//        Keywords
+        createProductPageAdm.fillField("keywords", createProductPageAdm.getProductName());
+
+//        Short Description
+        createProductPageAdm.fillField("short_description[en]", createProductPageAdm.getProductName());
+
+//        Description
+        createProductPageAdm.fillTextArea("trumbowyg-editor", String.valueOf(createProductPageAdm.getProductName()));
+
+//        Head Title
+        createProductPageAdm.fillField("head_title[en]", createProductPageAdm.getProductName());
+
+//        Meta Description
+        createProductPageAdm.fillField("meta_description[en]", createProductPageAdm.getProductName());
+
+//        Заполнение вкладки Information
+        createProductPageAdm.topMenuItemClick("Prices");
+
+//        Purchase Price
+        createProductPageAdm.fillField("purchase_price", "15,00");
+        createProductPageAdm.fillCombobox("purchase_price_currency_code", "US Dollars");
+
+//        Price
+        createProductPageAdm.fillField("prices[USD]", "5");
+        createProductPageAdm.fillField("gross_prices[EUR]", "5");
+
+//        Сохранение продукта
+        createProductPageAdm.buttonClick("save");
+        thread.sleep(2000);
+        Assert.assertTrue("Не было показано сообщение об успешном создании продукта", createProductPageAdm.notice_success().contains("Changes were successfully saved"));
+
+//        Поиск созданного товара
+        //        Переход на вкладку "Catalog"
+        mainAdminPage.mainMenuItemClick("Catalog");
+        mainAdminPage.subMenuItemClick("Catalog");
+
+        wait.until(ExpectedConditions.titleContains("Catalog | My Store"));
     }
 }
